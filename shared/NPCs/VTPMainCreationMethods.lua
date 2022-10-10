edd020918eaf6561576eb823f9fec549e546e26a
Lamim Rashid <lrashid1@myseneca.ca>
2022-10-10 06:24:18 -0400

    formatting

--- shared/NPCs/VTPMainCreationMethods.lua
+++ shared/NPCs/VTPMainCreationMethods.lua
@@ -1,14 +1,11 @@
 require('NPCs/MainCreationMethods');
 
 local function initVTPTraits()
-    local newbie = TraitFactory.addTrait("newbie", getText("UI_trait_newbie"), -1, getText("UI_trait_newbiedesc"),
-        false, false);
-    local spearhunter = TraitFactory.addTrait("spearhunter", getText("UI_trait_spearhunter"), 6,
-        getText("UI_trait_spearhunterdesc"), false, false);
+    local newbie = TraitFactory.addTrait("newbie", getText("UI_trait_newbie"), -1, getText("UI_trait_newbiedesc"), false, false);
+    local spearhunter = TraitFactory.addTrait("spearhunter", getText("UI_trait_spearhunter"), 6, getText("UI_trait_spearhunterdesc"), false, false);
     spearhunter:addXPBoost(Perks.Spear, 1);
     spearhunter:addXPBoost(Perks.Sneaking, 1);
-    local gunnut = TraitFactory.addTrait("gunnut", getText("UI_trait_gunnut"), 6, getText("UI_trait_gunspecialistdesc"),
-        false, false);
+    local gunnut = TraitFactory.addTrait("gunnut", getText("UI_trait_gunnut"), 6, getText("UI_trait_gunspecialistdesc"), false, false);
     gunnut:addXPBoost(Perks.Aiming, 1);
     gunnut:addXPBoost(Perks.Reloading, 1);
 end
